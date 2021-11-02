590a505366ba99a4ee03135d00d18ad26428d90f
Aditya Kumar <aryan68125@gmail.com>
2021-11-03 05:13:50 +0530

    Update MainActivity.java

--- app/src/main/java/com/aditya/scientificcalculator/MainActivity.java
+++ app/src/main/java/com/aditya/scientificcalculator/MainActivity.java
@@ -8,6 +8,52 @@ step 3 - now copy the jdk version
 step 4 - go to project menue and convert the project bar from android symbol to project symbol
 step 5 - paste the copied file in the libs folder that is present under build folder
 step 6 - now right click on the newly copied file and add the file as a library to your project
+
+
+in order to use gif images in android projects you have to add 
+step 1:- Add the lines below in build.gradel Project file
+```buildscript {
+    repositories {
+        google()
+        jcenter()
+        mavenCentral() **
+    }
+    dependencies {
+        classpath "com.android.tools.build:gradle:4.1.1"
+
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle files
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        jcenter()
+        mavenCentral() **
+    }
+}
+```
+
+step 2:- Add the dependency in you build.gradle Application file
+```` 
+
+implementation 'pl.droidsonroids.gif:android-gif-drawable:1.2.20'
+
+````
+
+step 2:- to use gif images in your activity xml files just type the following code:-
+```<pl.droidsonroids.gif.GifImageView
+        android:id="@+id/gifImageView"
+        android:layout_width="match_parent"
+        android:layout_height="0dp"
+        android:src="@drawable/loading_file"
+        app:layout_constraintBottom_toTopOf="@+id/SplashprogressBar"
+        app:layout_constraintEnd_toEndOf="parent"
+        app:layout_constraintStart_toStartOf="parent"
+        app:layout_constraintTop_toTopOf="parent" />
+        and you are good to go
+```
  */
 
 // import androidx.appcompat.widget.Toolbar; make sure to import this and not import android.widget.Toolbar;
